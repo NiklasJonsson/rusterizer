3887d0ae44f66f32c489911202ece63c9d7154a6
Niklas Jonsson <niklasandersjonsson@gmail.com>
2019-09-17 22:05:30 +0200

    Refactor into several files + add format param

---
+++ src/graphics_primitives.rs
@@ -0,0 +1,150 @@
+use core::ops::Add;
+use core::ops::Mul;
+
+use crate::math_primitives::*;
+
+#[derive(Debug, Copy, Clone)]
+pub struct Color {
+    pub r: f32,
+    pub g: f32,
+    pub b: f32,
+    pub a: f32,
+}
+
+impl Color {
+    pub fn to_rgba(&self) -> u32 {
+        (self.r * 255.0) as u32
+            | ((self.g * 255.0) as u32) << 8
+            | ((self.b * 255.0) as u32) << 16
+            | ((self.a * 255.0) as u32) << 24
+    }
+
+    pub fn to_bgra(&self) -> u32 {
+        (self.b * 255.0) as u32
+            | ((self.g * 255.0) as u32) << 8
+            | ((self.r * 255.0) as u32) << 16
+            | ((self.a * 255.0) as u32) << 24
+    }
+
+    pub fn red() -> Color {
+        Color {
+            r: 1.0,
+            g: 0.0,
+            b: 0.0,
+            a: 1.0,
+        }
+    }
+    pub fn green() -> Color {
+        Color {
+            r: 0.0,
+            g: 1.0,
+            b: 0.0,
+            a: 1.0,
+        }
+    }
+    pub fn blue() -> Color {
+        Color {
+            r: 0.0,
+            g: 0.0,
+            b: 1.0,
+            a: 1.0,
+        }
+    }
+}
+
+impl Mul<f32> for Color {
+    type Output = Color;
+
+    fn mul(self, scalar: f32) -> Color {
+        Color {
+            r: self.r * scalar,
+            g: self.g * scalar,
+            b: self.b * scalar,
+            a: self.a * scalar,
+        }
+    }
+}
+
+impl Add<Color> for Color {
+    type Output = Color;
+    fn add(self, other: Color) -> Color {
+        Color {
+            r: self.r + other.r,
+            g: self.g + other.g,
+            b: self.b + other.b,
+            a: self.a + other.a,
+        }
+    }
+}
+
+
+#[derive(Debug)]
+pub struct VertexAttribute {
+    color: Color,
+}
+
+impl From<Color> for VertexAttribute {
+    fn from(other: Color) -> VertexAttribute {
+        VertexAttribute {color: other}
+    }
+}
+
+#[derive(Debug)]
+pub struct Triangle {
+    pub vertices: [Point2D; 3],
+    normals: [Vec2D; 3],
+    pub vertex_attributes: [VertexAttribute; 3],
+    area: f32,
+}
+
+impl Triangle {
+    fn area(vertices: &[Point2D; 3]) -> f32 {
+        (vertices[1] - vertices[0]).cross(vertices[2] - vertices[0]) * 0.5
+    }
+
+    pub fn new(vertices: [Point2D; 3], vertex_attributes: [VertexAttribute; 3]) -> Triangle
+    {
+        // Clockwise edge equations
+        // To have the normals all pointing towards the inner part of the triangle,
+        // they all need to have their positive halfspace to the right of the triangle.
+        // If we wanted counter-clockwise, then we switch signs on both x and y of normals
+        // (and also switch order for v computations above. Note that coordinate system
+        // starts in upper left corner.
+
+        let v0 = vertices[1] - vertices[0];
+        let v1 = vertices[2] - vertices[1];
+        let v2 = vertices[0] - vertices[2];
+        let n0 = Vec2D::new(-v0.y, v0.x);
+        let n1 = Vec2D::new(-v1.y, v1.x);
+        let n2 = Vec2D::new(-v2.y, v2.x);
+
+        let normals = [n0, n1, n2];
+        let area = Triangle::area(&vertices);
+
+        Triangle {
+            vertices,
+            normals,
+            vertex_attributes,
+            area,
+        }
+    }
+
+   pub fn is_point_inside(&self, point: Point2D) -> bool {
+        // Based on edge equations
+        self.normals
+            .iter()
+            .zip(self.vertices.iter())
+            .fold(true, |acc, (&n, &p)| (n.dot(point - p) >= 0.0) && acc)
+    }
+
+    pub fn interpolate_color_for(&self, point: Point2D) -> Color {
+        assert!(self.is_point_inside(point));
+        let barycentric0 = Triangle::area(&[self.vertices[1], self.vertices[2], point]) / self.area;
+        let barycentric1 = Triangle::area(&[self.vertices[2], self.vertices[0], point]) / self.area;
+        let barycentric2 = 1.0 - barycentric0 - barycentric1;
+
+        self.vertex_attributes[0].color * barycentric0
+            + self.vertex_attributes[1].color * barycentric1
+            + self.vertex_attributes[2].color * barycentric2
+    }
+}
---
+++ src/rasterizer.rs
@@ -0,0 +1,121 @@
+use crate::graphics_primitives::*;
+use crate::math_primitives::*;
+
+use std::f32;
+
+struct PixelBoundingBox {
+    pub min_x: usize,
+    pub max_x: usize,
+    pub min_y: usize,
+    pub max_y: usize,
+}
+
+impl From<&[Point2D; 3]> for PixelBoundingBox {
+    fn from(vertices: &[Point2D; 3]) -> Self {
+        let vals = vertices
+            .iter()
+            .fold((f32::MAX, f32::MIN, f32::MAX, f32::MIN), |a, p| {
+                (a.0.min(p.x), a.1.max(p.x), a.2.min(p.y), a.3.max(p.y))
+            });
+        // Convert the min/max bounds into pixel coordinates. Always round
+        // away from the center of the box.
+        let (min_x, max_x, min_y, max_y) = (
+            vals.0.floor() as usize,
+            vals.1.ceil() as usize,
+            vals.2.floor() as usize,
+            vals.3.ceil() as usize,
+        );
+        assert!(min_x < max_x && min_y < max_y);
+        Self {
+            min_x,
+            max_x,
+            min_y,
+            max_y,
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+pub enum ColorBufferFormat {
+    RGBA,
+    BGRA,
+}
+
+// RGBA image
+#[derive(Debug)]
+pub struct ColorBuffer {
+    buffer: Vec<u32>,
+    width: usize,
+    height: usize,
+    format: ColorBufferFormat,
+}
+
+impl ColorBuffer {
+    pub fn new(width: usize, height: usize, format: ColorBufferFormat) -> Self {
+        let mut buffer = Vec::with_capacity(width * height);
+        // Initialize to black
+        for _i in 0..width * height {
+            buffer.push(0);
+        }
+        Self {
+            buffer,
+            width,
+            height,
+            format,
+        }
+    }
+
+    // Clear to black
+    fn clear(&mut self) {
+        assert_eq!(self.buffer.len(), self.height * self.width);
+        for i in 0..self.width * self.height {
+            self.buffer[i] = 0;
+        }
+    }
+
+    fn set_pixel(&mut self, row: usize, col: usize, color: Color) {
+        match self.format {
+            ColorBufferFormat::BGRA => self.buffer[row * self.width + col] = color.to_bgra(),
+            ColorBufferFormat::RGBA => self.buffer[row * self.width + col] = color.to_rgba(),
+
+        }
+    }
+
+    pub fn get_raw(&self) -> &Vec<u32> {
+        &self.buffer
+    }
+}
+
+pub struct Rasterizer {
+    color_buffer: ColorBuffer,
+}
+
+impl Rasterizer {
+    pub fn new(width: usize, height: usize) -> Self {
+        Self {
+            color_buffer: ColorBuffer::new(width, height, ColorBufferFormat::BGRA),
+        }
+    }
+
+    // Returns RGBA image
+    pub fn rasterize(&mut self, triangles: &[Triangle]) -> &ColorBuffer {
+        self.color_buffer.clear();
+        for triangle in triangles {
+            let bounding_box = PixelBoundingBox::from(&triangle.vertices);
+            for i in bounding_box.min_y..bounding_box.max_y {
+                for j in bounding_box.min_x..bounding_box.max_x {
+                    // Sample middle of pixel
+                    let x = j as f32 + 0.5;
+                    let y = i as f32 + 0.5;
+                    let pos = Point2D::new(x, y);
+                    if triangle.is_point_inside(pos) {
+                        let col = triangle.interpolate_color_for(pos);
+                        self.color_buffer.set_pixel(i, j, col);
+                    }
+                }
+            }
+        }
+
+        &self.color_buffer
+    }
+}
