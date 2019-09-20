dc599c8ffbf39473576b15fa4a53f01dd4896b87
Niklas Jonsson <niklasandersjonsson@gmail.com>
2019-09-20 18:36:03 +0200

    add point and vec in 3D

--- src/math_primitives.rs
+++ src/math_primitives.rs
@@ -42,3 +42,53 @@ impl Sub<Point2D> for Point2D {
         }
     }
 }
+
+#[derive(Debug, Copy, Clone)]
+pub struct Vec3D {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct Point3D {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
+impl Point3D {
+    pub fn new(x: f32, y: f32, z:f32) -> Self {
+        Self { x, y, z}
+    }
+}
+
+impl Vec3D {
+    pub fn new(x: f32, y: f32, z: f32) -> Self {
+        Self { x, y, z }
+    }
+
+    pub fn dot(self, other: Vec3D) -> f32 {
+        self.x * other.x + self.y * other.y + self.z * other.z
+    }
+
+    pub fn cross(self, other: Vec3D) -> Vec3D {
+        Vec3D {
+            x: self.y * other.z - self.z * other.y,
+            y: self.z * other.x - self.x * other.z,
+            z: self.x * other.y - self.y * other.x,
+        }
+    }
+}
+
+impl Sub<Point3D> for Point3D {
+    type Output = Vec3D;
+
+    fn sub(self, other: Point3D) -> Vec3D {
+        Vec3D {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        }
+    }
+}
