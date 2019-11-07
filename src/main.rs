0f1b367e017ddfd137290a74e50488d0930184a0
Niklas Jonsson <niklasandersjonsson@gmail.com>
2019-11-07 21:38:47 +0100

    WIP draw indirect

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,31 @@ use crate::rasterizer::*;
 const WIDTH: usize = 800;
 const HEIGHT: usize = 800;
 
+fn get_centered_quad(width: f32) -> (Vec<Vertex<WorldSpace>>,
+                                     Vec<VertexAttribute>,
+                                     Vec<usize>){
+    let color = Color::blue();
+    let vs = vec![
+        vertex(-width/2.0, width/2.0, 0.0),
+        vertex(width/2.0, width/2.0, 0.0),
+        vertex(width/2.0, -width/2.0, 0.0),
+        vertex(-width/2.0, -width/2.0, 0.0),
+    ];
+
+    let attrs = vec![color.into(); 4];
+
+    let indices = vec![
+        0, 2, 3, 0, 1, 2
+    ];
+
+    (vs, attrs, indices)
+}
+
 fn get_triangles() -> Vec<Triangle<WorldSpace>> {
-    let pos0 = vertex(0.3, 0.1, -3.0);
-    let pos1 = vertex(0.4, 0.3, -3.0);
-    let pos2 = vertex(0.2, 0.3, -3.0);
-    let color0 = Color::red();
+    let pos0 = vertex(1.0, 0.5, -2.0);
+    let pos1 = vertex(1.5, 1.5, -2.0);
+    let pos2 = vertex(0.5, 1.5, -2.0);
+    let color0 = Color::blue();
     let color1 = Color::red();
     let color2 = Color::red();
 
@@ -30,11 +50,10 @@ fn get_triangles() -> Vec<Triangle<WorldSpace>> {
         vertex_attributes,
     };
 
-    // Triangle 2, slightly shifted to the left
-    let pos0 = vertex(0.4, 0.1, -2.0);
-    let pos1 = vertex(0.5, 0.3, -2.0);
-    let pos2 = vertex(0.3, 0.3, -2.0);
-    let color0 = Color::green();
+    let pos0 = vertex(1.0, 0.5, 0.0);
+    let pos1 = vertex(1.5, 1.5, 0.0);
+    let pos2 = vertex(0.5, 1.5, 0.0);
+    let color0 = Color::red();
     let color1 = Color::green();
     let color2 = Color::green();
 
@@ -99,6 +118,7 @@ fn main() {
     let camera = get_camera();
     let triangles = get_triangles();
 
+
     let mut rasterizer = Rasterizer::new(WIDTH, HEIGHT);
 
     let mut window = Window::new(
@@ -126,9 +146,28 @@ fn main() {
         .map(|tri| proj_matrix * view_matrix * tri)
         .collect::<Vec<_>>();
 
+    let (quad_vs, quad_attrs, quad_indices) = get_centered_quad(2.0);
+    dbg!(&quad_vs);
+    dbg!(proj_matrix);
+    dbg!(view_matrix);
+    dbg!(proj_matrix * view_matrix);
+    let quad_vs = quad_vs
+        .into_iter()
+        .map(|vertex| proj_matrix * view_matrix * vertex)
+        .collect::<Vec<_>>();
+
+    let draw_quad = true;
+
+    dbg!(&quad_vs);
+
     while window.is_open() && !window.is_key_down(Key::Escape) {
         let t0 = Instant::now();
-        let color_buffer = rasterizer.rasterize(&triangles);
+        let color_buffer = if draw_quad {
+            rasterizer.draw_indirect(&quad_vs, &quad_attrs, &quad_indices)
+        } else {
+            rasterizer.draw(&triangles)
+        };
+
         avg = (avg * iterations + t0.elapsed()) / (iterations + 1);
         iterations += 1;
 
